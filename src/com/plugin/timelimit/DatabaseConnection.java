15c1d4ededc3a3d111d26b490bef707fadf42781
TillOttmann <till.ottmann@office.goetheschule-essen.de>
2024-10-24 11:35:06 +0200

    fixed issues: sql timeout, custom db name, unnecessary replace into

--- src/com/plugin/timelimit/DatabaseConnection.java
+++ src/com/plugin/timelimit/DatabaseConnection.java
@@ -13,7 +13,8 @@ class DatabaseConnection {
 	
 	private List<Integer> presetLimits;
 	private List<Boolean> presetStates;
-	private Connection connection;
+	private Connection conn;
+	private String DBName;
 
 	// Konstruktor, der die angegebenen Daten aus der config.yml benutzt, um die
 	// Datenbankverbindung aufzubauen
@@ -22,50 +23,74 @@ class DatabaseConnection {
 		presetLimits = new ArrayList<Integer>();
 		presetStates = new ArrayList<Boolean>();
 		FileConfiguration config = TimeLimitMain.getFileConfig();
-
+		Connection connection = null;
+		conn = null;
 		try {
 			String DBUrl = "jdbc:mysql://" + config.getString("DB_Url");
 			String DBUser = config.getString("DB_User");
 			String DBPw = config.getString("DB_Pw");
+			DBName = config.getString("DB_Name");
 			connection = DriverManager.getConnection(DBUrl, DBUser, DBPw);
 
-			TimeLimitMain.sendConsoleMessage("default", "Datenbankverbindung erfolgreich aufgebaut");
+			TimeLimitMain.sendConsoleMessage("default", "SQL-Serververbindung erfolgreich aufgebaut");
 
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		
 		presetLimits.add(config.getInt("GRADE_" + 0 + "_LIMIT"));
 		presetStates.add(config.getBoolean("GRADE_" + 0 + "_STATE"));
 		for (int i = 5; i <= 13; i++) {
 			presetLimits.add(config.getInt("GRADE_" + i + "_LIMIT"));
 			presetStates.add(config.getBoolean("GRADE_" + i + "_STATE"));
 		}
-		configureDatabase();
+		
+		configureDatabase(connection);
+		
+		try { 
+			connection.close();
+			TimeLimitMain.sendConsoleMessage("default", "SQL-Serververbindung erfolgreich geschlossen");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			TimeLimitMain.sendConsoleMessage("error", "Fehler beim schließen der SQL-Serververbindung");
+		}
+		
+		try {
+			String DBUrl = "jdbc:mysql://" + config.getString("DB_Url") + "/" + config.getString("DB_Name") + "?autoReconnect=true";
+			String DBUser = config.getString("DB_User");
+			String DBPw = config.getString("DB_Pw");
+			conn = DriverManager.getConnection(DBUrl, DBUser, DBPw);
+
+			TimeLimitMain.sendConsoleMessage("default", "Datenbankverbindung erfolgreich aufgebaut");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	// Gibt die Datenbankverbindung als Connection Objekt weiter
 	protected Connection getDatabaseConnection() {
-		return connection;
+		return conn;
 	}
 
-	private void configureDatabase() {
+	private void configureDatabase(Connection connection) {
 
 		try {
 			Statement stmt = connection.createStatement();
 
-			// Erstellt die Datenbank timelimit, falls diese nicht existiert
-			String createDatabase = "CREATE DATABASE IF NOT EXISTS timelimit";
+			// Erstellt die Datenbank, falls diese nicht existiert
+			String createDatabase = "CREATE DATABASE IF NOT EXISTS " + DBName;
 			stmt.executeUpdate(createDatabase);
 
 			if (stmt.getWarnings() != null) {
-				TimeLimitMain.sendConsoleMessage("default", "Datenbank 'timelimit' existiert bereits, Erstellung wird übersprungen");
+				TimeLimitMain.sendConsoleMessage("default", "Datenbank '" + DBName + "' existiert bereits, Erstellung wird übersprungen");
 
 			} else {
-				TimeLimitMain.sendConsoleMessage("warning", "Datenbank 'timelimit' erfolgreich erstellt");
+				TimeLimitMain.sendConsoleMessage("warning", "Datenbank '" + DBName + "' erfolgreich erstellt");
 			}
 
 			// Wählt die Datenbank aus
-			stmt.execute("USE timelimit");
+			stmt.execute("USE " + DBName);
 			stmt.clearWarnings();
 
 			// Erstellt die presetData Tabelle, diese enthält das voreingestellte Zeitlimit
@@ -117,21 +142,22 @@ class DatabaseConnection {
 			}
 			
 			// Fügt Zeitlimit-Presets in die Tabelle ein, falls diese nicht existieren
-			String insertOrUpdatePresets = "REPLACE INTO presetData (grade, timelimit, status) VALUES ";
+			String insertOrUpdatePresets = "INSERT IGNORE presetData (grade, timelimit, status) VALUES ";
 			for (int i = 0; i <= (13 - 5 + 1); i++) {
 				insertOrUpdatePresets += "(" + i  + "," + presetLimits.get(i)  + "," + presetStates.get(i) + "),";
 			}
 			int affectedRows = stmt.executeUpdate(insertOrUpdatePresets.substring(0, insertOrUpdatePresets.length() -1));
 			
-			if (affectedRows == 20) {
-				TimeLimitMain.sendConsoleMessage("default", "Alle Limit-Presets erfolgreich aktualisiert");
+			if (affectedRows == 0) {
+				TimeLimitMain.sendConsoleMessage("default", "Alle Limit-Presets vorhanden");
 
-			} else {
-				TimeLimitMain.sendConsoleMessage("warning", "INGORE IF FIRST TIME SETUP: Nicht alle Limit-Presets konnten eingefügt werden");
+			} else if (affectedRows == 10) {
+				TimeLimitMain.sendConsoleMessage("warning", "Fehlende limit-presets wurden hinzugefügt");
 			}
 
 			// Schliesst das Statement
 			stmt.close();
+
 
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -142,14 +168,14 @@ class DatabaseConnection {
 	// aufgerufen
 	protected void closeDatabaseConnection() {
 		
-		if (connection != null) {
+		if (conn != null) {
 			try {
-				connection.close();
+				conn.close();
 				TimeLimitMain.sendConsoleMessage("default", "Datenbankverbindung erfolgreich geschlossen");
 
 			} catch (SQLException e) {
 				e.printStackTrace();
-
+				TimeLimitMain.sendConsoleMessage("error", "Fehler beim schließen der Datenbankverbindung");
 			}
 		}
 	}
